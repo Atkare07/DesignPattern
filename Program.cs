7578fead5aa5b398a2ba9c121631fb1f201f6c82
Atkare07 <96227615+Atkare07@users.noreply.github.com>
2024-10-13 13:03:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DemoApplicationForDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 != s2)
+            {
+                Console.WriteLine("Not sharing the same resourece");
+            }
+            else
+            {
+                Console.WriteLine("Sharing the same resource");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DemoApplicationForDesignPattern
+{
+    internal class Singleton
+    {
+        // Declare private static field
+        private static Singleton instance;
+
+        // Private constructor
+        private Singleton() { }
+
+        // GetInstance method which will be accessed from outside scope
+        // will return instance always when invoked
+        // if first time call create instance
+        // else return already created
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance; // Corrected to return the instance
+        }
+    }
+}
